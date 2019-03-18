656c740e05d716a4a6b15c3a76a30b474e5b46f3
Brandon Fulljames <bfullj@gmail.com>
2019-03-17 22:04:20 -0700

    More board tests

--- lib/game/tictactoe.test.ts
+++ lib/game/tictactoe.test.ts
@@ -76,6 +76,63 @@ describe('TicTacToe', () => {
 
                 expect(ttt.getResult()).to.equal(Result.X);
             }
+
+            // Make sure they all have to match
+            for (let x = 0; x < 3; ++x) {
+                const ttt = new TicTacToe();
+
+                const squares = ttt.getSquares();
+
+                squares[0][x] = squares[2][x] = Player.X;
+                squares[1][x] = Player.O;
+
+                expect(ttt.getResult()).to.equal(Result.Unfinished);
+            }
+        });
+
+        it('finds diagonal down wins', () => {
+            const ttt = new TicTacToe();
+
+            const squares = ttt.getSquares();
+
+            squares[0][0] = Player.O;
+            squares[1][1] = Player.O;
+            squares[2][2] = Player.O;
+
+            expect(ttt.getResult()).to.equal(Result.O);
+        });
+
+        it('finds diagonal up wins', () => {
+            const ttt = new TicTacToe();
+
+            const squares = ttt.getSquares();
+
+            squares[0][2] = Player.O;
+            squares[1][1] = Player.O;
+            squares[2][0] = Player.O;
+
+            expect(ttt.getResult()).to.equal(Result.O);
+        });
+
+        it('draws when the board is full', () => {
+            const ttt = new TicTacToe();
+
+            // X | O | X
+            // X | X | O
+            // O | X | O
+
+            // The worst game of Tic Tac Toe to ever be played
+            ttt.move(0, 0); // X
+            ttt.move(1, 0); // O
+            ttt.move(2, 0); // X
+            ttt.move(2, 1); // O
+            ttt.move(0, 1); // X
+            ttt.move(2, 2); // O
+            ttt.move(1, 1); // X
+            ttt.move(0, 2); // O
+            ttt.move(1, 2); // X
+
+            expect(ttt.getResult()).to.equal(Result.Draw);
         });
     });
 });
